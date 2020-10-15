b33baa0b6c534525fc06f7322d8f76f9965cbbe2
Ayushi <ayushigoyal2840@gmail.com>
2020-10-15 16:27:00 +0530

    making my django project added some html files in it for making pages

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -10,6 +10,19 @@ urlpatterns = [
     path('search/', views.search, name="Search"),
     path('productview/', views.productView, name="Search"),
     path('checkout/', views.checkout, name="Checkout"),
+    path('login/', views.login, name="Login"),
+    path('shop/', views.shop, name="Shop"),
+    path('cart/', views.cart, name="Cart"),
+    path('checkout/', views.checkout, name="Checkout"),
+    path('blog/', views.blog, name="Blog"),
+    path('blog-details/', views.blog_details, name="Blog Details"),
+    path('confirmation/', views.confirmation, name="Confirmation"),
+    path('product_details/', views.product_details, name="product_details"),
+    path('test/', views.test, name="test"),
+    path('basic/', views.basic, name="basic"),
+    # path('product/', views.product, name="product"),
+
+        
     
 
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .models import Product
 def index (request):
     return render(request, 'shop/index.html')
 def about (request):
     return render(request, 'shop/about.html')
 def contact (request):
-    return HttpResponse("We are contact")
+    return render(request, 'shop/contact.html')
 def tracker (request):
     return HttpResponse("We are tracker")
 def search (request):
     return render(request, 'shop/search.html')
 def productView (request):
-    return HttpResponse("We are view")
+    return render(request, 'shop/productView.html')
 def checkout (request):
-    return HttpResponse("We are out")
+    return render(request, 'shop/checkout.html')
+def login(request):
+    return render(request, 'shop/login.html')
+def shop(request):
+    return render(request, 'shop/shop.html')
+def cart(request):
+    return render(request, 'shop/cart.html')
+def blog(request):
+    return render(request, 'shop/blog.html')
+def blog_details(request):
+    return render(request, 'shop/blog-details.html')
+def confirmation(request):
+    return render(request, 'shop/confirmation.html')
+def product_details(request):
+    return render(request, 'shop/product_details.html')
+def test(request):
+    return render(request, 'shop/test.html')
+def basic(request):
+    return render(request, 'shop/basic.html')
+
+
