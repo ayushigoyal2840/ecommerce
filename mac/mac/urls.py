d422d25921532793b68807314afbb8a5fea0e45f
Ayushi <ayushigoyal2840@gmail.com>
2020-11-08 13:29:04 +0530

    changed log in image and doen some modificaitons in main page

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop import views
+
 urlpatterns = [
+    path('',views.index),
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Exclusive shop Admin"
+admin.site.site_title = "Exclusive shop Admin Portal"
+admin.site.index_title = "Welcome to Exclusive shop Portal"
